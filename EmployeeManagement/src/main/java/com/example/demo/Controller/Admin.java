41f4d84cd3474db8b95f154c7b99b7df9e651446
khushbuchawda14@gmail.com <#Khush@07>
2025-07-04 14:16:27 +0530

    one api added

---
+++ EmployeeManagement/src/main/java/com/example/demo/Controller/Admin.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Admin {
+	@GetMapping("/add")
+	public String addAdmin()
+	{
+		System.out.println("Admin Added");
+		return "admin added";
+	}
+
+}
